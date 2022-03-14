204c91d186818a73c5fd34a5d517e71fb0c6b264
LawrenceDeacon <lawdeacon.ld@gmail.com>
2022-03-14 11:26:30 +0000

    Added clearGrid function

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 const wrapper = document.querySelector('.wrapper');
 
 let divs = [];
-for (let index = 0; index < 256; index++) {
-    divs[index] = "div"+index;
-}
+function clearGrid(size = 16) {
+    let gridSize = size * size;
+    divs = [];
+    
+    for (let index = 0; index < gridSize; index++) {
+        divs[index] = "div"+index;
+    }
 
-divs.forEach(element => {
-    wrapper.append(document.createElement('div'));
-});
+    divs.forEach(element => {
+        wrapper.remove(document.createElement('div'));
+    });
+    
+    divs.forEach(element => {
+        wrapper.append(document.createElement('div'));
+    });
+    
+    console.log(divs);
+}clearGrid();
 
-console.log(divs);
+// clear listen
+const clear = document.querySelector('button').addEventListener('click', function () {
+    let size = prompt("Please enter the grid size, ie 8 for an 8X8 grid. Max 16X16, even numbers only above 3");
+    if (size > 16) {
+        size = prompt("Please enter a number < than 16");
+    }if (size < 4) {
+        size = prompt("Please enter a number > 3");
+    }if (size % size == 1) {
+        size = prompt("Please enter an even number!");
+    }if (size = 0) {
+        return;
+    }
+    clearGrid(size);
+})
+
+
